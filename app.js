2b5d194a246724093b90cf0022c0f6ee2b2baae9
naughtLdy <sdax05@gmail.com>
2017-03-21 15:26:54 +0900

    initialize commit

---
+++ app.js
@@ -0,0 +1,29 @@
+let express = require('express');
+let path = require('path');
+
+let app = express();
+let env = app.get('env');
+let port = 7000;
+app.set('port', port);
+
+// production
+if (env === "production") {
+  app.use(express.static('dist'));
+
+  app.listen(port, () => {
+    console.log("app listening at http://localhost:%d mode:%s", port, env)
+  });
+}
+
+// development
+if (env === "development") {
+  let index = require('./routes/index');
+  app.use('/', index);
+
+  app.set('views', path.join(__dirname, 'src', 'pug'));
+  app.set('view engine', 'pug')
+  
+  app.listen(port, () => {
+    console.log("app listening at http://localhost:%d mode:%s", port, env)
+  });
+}
